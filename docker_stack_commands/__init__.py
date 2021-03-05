45410573551d9d7eb84486176006628a278acc5d
Zeke Marffy <zmarffy@me.com>
2021-03-05 23:20:55 +0000

    remove old code; add requirement

--- docker_stack_commands/__init__.py
+++ docker_stack_commands/__init__.py
@@ -1,3 +1,12 @@
+from reequirements import Requirement
+
 from .api import *
 
 __version__ = "1.0.0"
+
+
+REQUIREMENTS = [
+    Requirement("docker", ["docker", "-v"]),
+]
+for requirement in REQUIREMENTS:
+    requirement.check()
--- setup.py
+++ setup.py
@@ -1,32 +1,25 @@
+import os
+import re
+
 import setuptools
-import zetuptools
-from reequirements import Requirement
 
-REQUIREMENTS = [
-    Requirement("docker", ["docker", "-v"]),
-]
-for requirement in REQUIREMENTS:
-    requirement.check()
-
-TOOLS = zetuptools.SetuptoolsExtensions(
-    "docker-stack-commands", "Zeke Marffy", "zmarffy@yahoo.com")
-
+with open(os.path.join("docker_stack_commands", "__init__.py"), encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setuptools.setup(
-    name=TOOLS.name,
-    version=TOOLS.version,
-    author=TOOLS.author,
-    author_email=TOOLS.author_email,
+    name="docker-stack-commands",
+    version=version,
+    author="Zeke Marffy",
+    author_email="zmarffy@yahoo.com",
     packages=setuptools.find_packages(),
     url='https://github.com/zmarffy/docker-stack-commands',
     license='MIT',
     description='Minimal and limited Python API for dealing with Docker stacks',
-    python_requires=TOOLS.minimum_version_required,
+    python_requires='>=3.6',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
         'reequirements',
-        'zmtools>=1.4.0',
-        'zetuptools>=2.3.0'
+        'zmtools>=1.4.0'
     ],
 )
